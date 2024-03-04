43d081ddbe8c791496db0b8cf04a00db804bdd98
Tekpakma <github@kaso.dev>
2024-03-04 22:32:56 +0100

    Changes

--- src/services/auth/email_login.js
+++ src/services/auth/email_login.js
@@ -1,32 +1,36 @@
 import { auth, db } from "../firebase.config";
-import { addDoc, collection } from "firebase/firestore";
 import {
-  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+import toast from "react-hot-toast";
 
 export const logInWithEmailAndPassword = async (email, password) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
   } catch (err) {
     console.error(err);
-    alert(err.message);
+    toast.error(err.message);
   }
 };
 export const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users"), {
+    await setDoc(doc(db, "users", user.uid), {
       uid: user.uid,
       name,
       authProvider: "local",
       email,
     });
+    await updateProfile(user, { displayName: name });
   } catch (err) {
     console.error(err);
-    alert(err.message);
+    toast.error(err.message);
   }
 };
 export const sendPasswordReset = async (email) => {
@@ -35,6 +39,6 @@ export const sendPasswordReset = async (email) => {
     alert("Password reset link sent!");
   } catch (err) {
     console.error(err);
-    alert(err.message);
+    toast.error(err.message);
   }
 };
